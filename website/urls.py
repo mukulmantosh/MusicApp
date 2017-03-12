d01d47eb29ac3b76710288cfb5397c206b1f4dd4
Mukul Mantosh <mukulmantosh@yahoo.com>
2017-03-12 15:04:52 +0530

    added file uploading logic

--- website/urls.py
+++ website/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -23,3 +25,8 @@ urlpatterns = [
     url(r'^music/', include('music.urls')),
     
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
