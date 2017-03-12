867d23c92fcaeaa54a0ee27dbbc6c38280af08b5
Mukul Mantosh <mukulmantosh@yahoo.com>
2017-03-12 21:10:28 +0530

    Added Login Authentication System

--- music/urls.py
+++ music/urls.py
@@ -14,6 +14,9 @@ app_name = 'music'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view() , name='index'),
+    url(r'^register/$', views.UserFormView.as_view() , name='register'),
+    url(r'^login/$', views.LoginFormView.as_view() , name='login'),
+    url(r'^logout/$', views.LogoutView.as_view() , name='logout'),
     url(r'^(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='detail'),  
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
     url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
--- music/views.py
+++ music/views.py
@@ -49,6 +49,10 @@ from django.views import generic
 from .models import Album
 from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login , logout
+from django.views.generic import View
+from .forms import UserForm, LoginForm
 
 
 class IndexView(generic.ListView):
@@ -79,3 +83,84 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
 	model = Album	
 	success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+	form_class = UserForm
+	template_name = 'music/registration_form.html'
+
+
+	def get(self,request):
+		form = self.form_class(None)
+		return render(request, self.template_name, {'form':form})
+
+
+
+	# process form data	
+	def post(self,request):
+		form = self.form_class(request.POST)
+
+
+		if form.is_valid():
+			user = form.save(commit=False)
+
+	
+			# cleaned data
+			username = form.cleaned_data['username']
+			email 	 = form.cleaned_data['email']
+			password = form.cleaned_data['password']
+			user.set_password(password)
+			user.save()
+
+			# returns user objects if credentials are correct
+			user = authenticate(username=username,password=password)
+
+			if user is not None:
+
+				if user.is_active:
+
+					login(request,user)
+					return redirect('music:index')
+
+		return render(request, self.template_name, {'form':form})
+
+
+
+
+# Logic for LoggingIn
+
+class LoginFormView(View):
+	form_class = LoginForm
+	template_name = 'music/login_form.html'
+
+
+	def get(self,request):
+		if request.user.is_authenticated():
+			return redirect('music:index')			
+		else:
+			form = self.form_class(None)
+			return render(request, self.template_name, {'form':form})
+
+
+
+
+	# process login-form data	
+	def post(self,request):
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+
+		if user is not None:
+			 login(request, user)
+			 return redirect('music:index')
+
+		return render(request, self.template_name, {'form':form})
+
+
+
+class LogoutView(View):
+
+    def get(self, request):
+    	logout(request)
+    	return redirect('music:index')
+
